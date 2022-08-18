40ae27e699335b160c59bc397f39f291297d8837
austineoduor <austineoduor57@gmail.com>
2022-08-18 14:38:13 +0300

    file

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -15,9 +15,9 @@ if (process.argv.length <= 2) {
 
 const size = parseInt(process.argv[2], 16);
 
-for (let i = 0; i < size; i ++) {
-  for (let j = 0; j < size; j ++) {
-	process.stdout.write("#");
+for (let i = 0; i < size; i++) {
+  for (let j = 0; j < size; j++) {
+  process.stdout.write("#");
   }
   process.stdout.write("\n");
 }
